074967e5b9861cd739f21bfbb653cad4c574e126
Maddie Wagner <musomaddy@gmail.com>
2019-05-20 14:48:19 +1000

    added comments for day of week

--- visualise_data/day_of_week.py
+++ visualise_data/day_of_week.py
@@ -7,5 +7,12 @@ sys.path.insert(0,
 
 from process_data.read_data import make_projects
 
+
+def bar_graph(projects):
+    """ Creates a bar graph showing the number of hours of work grouped by the
+    days of the week """
+    pass
+
+
 projects = make_projects()
 print(projects)
